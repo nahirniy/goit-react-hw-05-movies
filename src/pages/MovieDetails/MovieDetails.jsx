390d4ccad141fe721ca0062eb40ba1bb4548e765
nahirniy <nahirniyy@gmail.com>
2023-12-26 12:14:17 +0200

    create details cart page

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDetailsMovie } from 'services/movies-api';
 import MovieDetailsCart from './MovieDetailsCart/MovieDetailsCart';
+import Loader from 'components/Loader/Loader';
+import Error from 'components/Error/Error';
 
 const STATUS = {
   IDLE: 'idle',
@@ -19,6 +21,8 @@ const MovieDetails = () => {
   const { movieId } = useParams();
 
   useEffect(() => {
+    setStatus(STATUS.PENDING);
+
     const fetchDetailsMovie = async () => {
       const movie = await getDetailsMovie(movieId);
 
@@ -34,7 +38,13 @@ const MovieDetails = () => {
     fetchDetailsMovie();
   }, [movieId]);
 
-  return <MovieDetailsCart details={detailsMovie} />;
+  return (
+    <>
+      {status === STATUS.RESOLVED && <MovieDetailsCart details={detailsMovie} />}
+      {status === STATUS.PENDING && <Loader />}
+      {status === STATUS.REJECTED && <Error message="Load failed details of movie" />}
+    </>
+  );
 };
 
 export default MovieDetails;
