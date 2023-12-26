4d9e83a5be05a50c720dfa48a0e283ec11699cde
nahirniy <nahirniyy@gmail.com>
2023-12-26 16:38:12 +0200

    update cast part

--- src/components/MovieDetailsContent/AddContent/Cast/CastList.jsx
+++ src/components/MovieDetailsContent/AddContent/Cast/CastList.jsx
@@ -2,7 +2,7 @@ import css from './Cast.module.css';
 
 const CastList = ({ cast }) => {
   const BASE_IMG = 'https://image.tmdb.org/t/p/w500';
-  console.log(Boolean(cast.length));
+
   return (
     <>
       <h2 className={css.cast_title}>Cast:</h2>
